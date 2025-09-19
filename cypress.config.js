4de5a5f0c5745253c823229376fd723aa9f7685a
Matheus <matheus_bereta@hotmail.com>
2025-09-18 22:14:18 -0300

    :fire: Removing code from cypress.config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'http://localhost:3000',
     experimentalStudio: true,
     fixturesFolder: false,
   },
-});
+})
